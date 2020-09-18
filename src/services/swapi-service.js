99ab8ace347dff717200987e8ea99e28c6c4b9af
Artem <temaspsxxx@gmail.com>
2020-09-19 00:20:11 +0300

    feat: add transform data methods

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -11,28 +11,74 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource(`/people/`);
-    return res.results;
+    return res.results.map(this._transformPerson);
   }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}/`);
+    return this._transformPerson(person)
   }
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getResource(`/starships/`);
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
+  async getStarship(id) {
+    const starship = await this.getResource(`/starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/(\d+)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet(planet) {
+    const { name, population, rotation_period: rotationPeriod, diameter } = planet;
+
+    return {
+      id: this._extractId(planet),
+      name,
+      population,
+      rotationPeriod,
+      diameter,
+    }
+  }
+
+  _transformStarship(starship) {
+    const { name, model, manufacturer, costInCredits, crew, passengers, cargoCapacity } = starship;
+    return {
+      id: this._extractId(starship),
+      name,
+      model,
+      manufacturer,
+      costInCredits,
+      length: starship.length,
+      crew,
+      passengers,
+      cargoCapacity,
+    }
+  }
+
+  _transformPerson(person) {
+    const { name, gender, birthYear, eyeColor } = person;
+    return {
+      id: this._extractId(person),
+      name,
+      gender,
+      birthYear,
+      eyeColor,
+    }
   }
 }
