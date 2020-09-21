5c463115d7b5a1548a2f92ce68e5162397c8d95a
Artem <temaspsxxx@gmail.com>
2020-09-21 21:03:03 +0300

    feat: add service for pass funtions to components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,10 +5,15 @@ import RandomPlanet from '../random-planet'
 import PeoplePage from '../people-page'
 import ErrorIndicator from '../error-indicator'
 import ErrorButton from '../error-button';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import SwapiService from '../../services/swapi-service'
 
 import './app.css'
 
 class App extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true,
@@ -48,8 +53,28 @@ class App extends Component {
         </div>
 
         <PeoplePage />
-        <PeoplePage />
-        <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     )
   }
