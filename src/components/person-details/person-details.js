41da6a7b00fe706be6e1560cf3b8e806fef22576
Artem <temaspsxxx@gmail.com>
2020-09-21 21:05:44 +0300

    feat: add error button for throw error

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service'
+import ErrorButton from '../error-button';
 
 import './person-details.css';
 
@@ -59,6 +60,7 @@ export default class PersonDetails extends Component {
               <span>{eyeColor}</span>
             </li>
           </ul>
+          <ErrorButton />
         </div>
       </div>
     )
