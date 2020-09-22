c022312927b20d17358a9c65c006be6b4aad1694
Artem <temaspsxxx@gmail.com>
2020-09-22 22:46:08 +0300

    refactor: move element to Row component props

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -4,6 +4,7 @@ import ItemList from '../item-list'
 import PersonDetails from '../person-details'
 import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../services/swapi-service';
+import Row from '../row';
 
 import './people-page.css';
 
@@ -31,18 +32,15 @@ export default class PeoplePage extends Component {
 
     if (this.state.hasError) return <ErrorIndicator />
 
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({ name, gender, birthYear }) => `${name} (${gender}, ${birthYear})`} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, gender, birthYear }) => `${name} (${gender}, ${birthYear})`} />
     )
+
+    const personDetails = <PersonDetails personId={this.state.selectedPerson} />
+
+    return <Row left={itemList} right={personDetails} />
   }
 }
