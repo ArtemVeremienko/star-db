6f09ef21a434d75fba1563ab71396ef943380f11
Artem <temaspsxxx@gmail.com>
2020-09-22 23:13:50 +0300

    feat: add component for boundry error

---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import ErrorIndicator from '../error-indicator';
+
+import './error-boundry.css';
+
+export default class ErrorBoundry extends Component {
+
+  state = {
+    hasError: false
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true
+    })
+    console.log(error, info)
+  }
+
+  render() {
+    if (this.state.hasError) return <ErrorIndicator />
+    return this.props.children
+  }
+}
