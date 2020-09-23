891eadb7e86f315d4c29a0695c7af909deecef7c
Artem <temaspsxxx@gmail.com>
2020-09-23 17:33:43 +0300

    feat: add HOC component for render ItemList

---
+++ src/components/hoc-helpers/with-data.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import Spinner from '../spinner';
+import ErrorBoundry from '../error-boundry/error-boundry';
+
+export const withData = (View, getData) => {
+  return class extends Component {
+    state = {
+      data: null
+    };
+
+    componentDidMount() {
+      getData()
+        .then((data) => {
+          this.setState({ data })
+        })
+    }
+
+    render() {
+      const { data } = this.state;
+      if (!data) return <Spinner />
+
+      return (
+        <ErrorBoundry>
+          <View {...this.props} data={data} />
+        </ErrorBoundry>
+      );
+    }
+  };
+};
