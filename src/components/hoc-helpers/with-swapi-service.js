398d23f03bd98753c3f8ea6e528f58f06cf2a50d
Artem <temaspsxxx@gmail.com>
2020-09-25 23:00:45 +0300

    refactor: add HOC map method to props

--- src/components/hoc-helpers/with-swapi-service.js
+++ src/components/hoc-helpers/with-swapi-service.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { SwapiServiceConsumer } from '../swapi-service-context'
 
-const withSwapiService = (Wrapped) => {
+const withSwapiService = (Wrapped, mapMethodsToProps) => {
   return (props) =>
     <SwapiServiceConsumer>
       {
-        (swapiService) =>
-          <Wrapped {...props} swapiService={swapiService} />
+        (swapiService) => {
+          const serviceProps = mapMethodsToProps(swapiService)
+          return < Wrapped {...props} {...serviceProps} />
+        }
       }
     </SwapiServiceConsumer>
 }
