ff43b17dfff11d2d3e1b46860bbd795fb659d217
Artem <temaspsxxx@gmail.com>
2020-09-28 12:10:16 +0300

    fix: rename files and re-exports

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,3 +1,3 @@
 export { default as PeoplePage } from './people-page'
-export { default as PlanetsPage } from './planet-page'
-export { default as StarshipsPage } from './starship-page'
+export { default as PlanetsPage } from './planets-page'
+export { default as StarshipsPage } from './starships-page'
